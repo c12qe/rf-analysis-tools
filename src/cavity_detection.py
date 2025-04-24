8187be7431750d3477bb5ab85168ab3f005bea80
DebopamDatta <debopam.datta@c12qe.com>
2025-04-24 12:28:50 +0200

    Modification in cavity detection script.

--- src/cavity_detection.py
+++ src/cavity_detection.py
@@ -13,10 +13,11 @@ from scipy.signal import find_peaks, welch
 from typing import Tuple, List, Dict, Any
 
 # Import scraps
+import scraps as scr
+from scraps import Resonator
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scraps import Resonator
 import pandas as pd
 import os
 from scipy.optimize import curve_fit
@@ -25,46 +26,71 @@ from scipy.optimize import curve_fit
 
 class Sij_data():
         # Extract amplitude and phase from S21 data
-        def __init__(self, db_path, run_id: int = None):
+    def __init__(self, db_path, run_id: list = None):
 
-            self.db_path = db_path
-            self.run_id = run_id
-            self.pna_frequency = None
-            self.data_variable_keys = None
-            self.linear_magnitude = None
-            self.phase = None
-            self.unwrapped_phase = None
-            self.dB_magnitude= None
-            self.I=None
-            self.Q = None
+        self.db_path = db_path
+        self.all_runs = run_id
+        self.pna_frequency = None
+        self.linear_magnitude = None
+        self.phase = None
+        self.unwrapped_phase = None
+        self.dB_magnitude= None
+        self.I= None
+        self.Q= None
 
-    def Load_Single_Data(self):
+    def load_single_data(self):
+        '''
+        Loads single datafile or multiple datafiles from the database and concatenated.
+        :return: None
+        '''
         assert self.run_id >= 0, "Run ID must be provided"
         # Load the database
         initialise_or_create_database_at(self.db_path)
         # Load the dataset
-        if self.run_id is None:
-            print('Experiment list: 'experiments(initialise_or_create_database_at(self.db_path)))
-            val = int(input('Input run id you want to load'))
-            self.run_id = val
 
-        data = load_by_run_spec(captured_run_id=self.run_id)
-        ds = data.to_xarray_dataset()
+        freq = np.array([])
+        linear_magnitude =np.array([])
+        db_magnitude = np.array([])
+        phase = np.array([])
+        unwrapped_phase = np.array([])
+        i = np.array([])
+        q = np.array([])
 
-        if ds.coords in ['pna_frequency_axis']:
-            self.pna_frequency = ds['pna_frequency_axis'].values
+        for indx_ii,run_id in enumerate(self.all_runs):
+            data = load_by_run_spec(captured_run_id=run_id)
+            ds = data.to_xarray_dataset()
 
-        data_variables = list(ds.data_vars.keys())
-        self.linear_magnitude = ds.data_vars['pna_tr1_linear_magnitude'].data
-        self.phase = ds.data_vars['pna_tr1_phase'].data
-        self.unwrapped_phase = ds.data_vars['pna_tr1_unwrapped_phase'].data
-        self.dB_magnitude = ds.data_vars['pna_tr1_magnitude'].data
+            if indx_ii == 0:
+                freq = ds['pna_frequency_axis'].values
+                linear_magnitude = ds.data_vars['pna_tr1_linear_magnitude'].data
+                phase = ds.data_vars['pna_tr1_phase'].data
+                unwrapped_phase = ds.data_vars['pna_tr1_unwrapped_phase'].data
+                db_magnitude = ds.data_vars['pna_tr1_magnitude'].data
+                total_transmission = linear_magnitude * np.exp(1j * phase * np.pi/180)
+                i = np.real(total_transmission)
+                q = np.imag(total_transmission)
+            else:
+                indv_magnitude = ds.data_vars['pna_tr1_linear_magnitude'].data
+                indv_phase = ds.data_vars['pna_tr1_phase'].data
 
-        total_transmission = self.linear_magnitude * np.exp(1j * self.phase * np.pi/180)
-        self.I = np.real(total_transmission)
-        self.Q = np.imag(total_transmission)
+                freq = np.concatenate((freq, ds['pna_frequency_axis'].values))
+                linear_magnitude = np.concatenate((linear_magnitude, indv_magnitude))
+                phase = np.concatenate((phase, indv_phase))
+                unwrapped_phase = np.concatenate((unwrapped_phase, ds.data_vars['pna_tr1_unwrapped_phase'].data))
+                db_magnitude = np.concatenate((db_magnitude, ds.data_vars['pna_tr1_magnitude'].data))
+                iq = indv_magnitude * np.exp(1j*indv_phase*np.pi/180)
 
-    def Load_Multiple_Data(self, run_ids: List[int]):
+                i = np.concatenate((i, np.real(iq)))
+                q = np.concatenate((q, np.imag(iq)))
+
+        self.pna_frequency = freq
+        self.linear_magnitude = linear_magnitude
+        self.phase = phase
+        self.unwrapped_phase = unwrapped_phase
+        self.dB_magnitude = db_magnitude
+        self.I = i
+        self.Q = q
+
 
 
     def prepare_runfile_for_qumin(self, setup_file_path):
